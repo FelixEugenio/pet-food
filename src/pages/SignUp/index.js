db2d9bcb6de2512eca5619302524c330ff802445
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-10-09 19:08:27 +0100

    feat: buildind signup page

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const SignUp = () => {
+
+    return (
+        <div className="Container-fluid h-100 bg-primary">
+            <div className="col-12">
+                <img src={"logo512.png"} className="img-fluid" alt="Logo" />
+            </div>
+        </div>
+    )
+}
