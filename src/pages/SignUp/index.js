3405abe58b07bcf62f5f0f8845ee873afe7cdfe9
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-10-09 20:21:18 +0100

    feat: sigup form

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,35 @@
 import React from "react"
+import logo from "../../assets/logo.png"
+import bg from "../../assets/bg.png"
 
 export const SignUp = () => {
 
     return (
         <div className="Container-fluid h-100 bg-primary">
             <div className="col-12">
-                <img src={"logo512.png"} className="img-fluid" alt="Logo" />
+                <img src={logo}  alt="Logo" />
             </div>
+            <div className="row">
+               
+            <div className="col-6">
+                <img src={bg}  alt="Logo" />
+            </div>
+
+            <div className="col-6">
+            <div className="box">
+                <h2>Falta pouco pra fazer pra seu pet feliz.</h2>
+                <input type="text" className="form-control form-control-lg mb-3" placeholder="Nome completo" />
+                <input type="text" className="form-control form-control-lg mb-3" placeholder="Email" />
+                <input type="text" className="form-control form-control-lg mb-3" placeholder="Telefone" />
+                <input type="text" className="form-control form-control-lg mb-3" placeholder="Nif" />
+                <input type="date" className="form-control form-control-lg mb-3" placeholder="data de nascimento" />
+                <button className="btn btn-primary btn-lg btn-block">Cadastrar</button>
+           </div>
+            </div>
+
+            </div>
+
+           
         </div>
     )
 }
